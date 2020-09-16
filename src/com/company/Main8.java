6feb6af755e8205aa834e58d2e1154e2db67437c
Pacal2 <i-jackie@hotmail.com>
2020-09-16 18:46:21 +0200

    GridBagConstraints

--- src/com/company/Main8.java
+++ src/com/company/Main8.java
@@ -41,7 +41,13 @@ public class Main8 extends JFrame {
         gridBagConstraints.weightx = 50;
         gridBagConstraints.weighty = 100;
         gridBagConstraints.insets = new Insets(5, 5, 5, 5);
-        
+        gridBagConstraints.anchor = GridBagConstraints.CENTER;
+        gridBagConstraints.fill = GridBagConstraints.BOTH;
+
+
+        textResult = new JTextField("0", 20);
+        Font font = new Font("Helvetica", Font.PLAIN, 18);
+        textResult.setFont(font);
 
         button1 = new JButton("1");
         button2 = new JButton("2");
@@ -55,19 +61,42 @@ public class Main8 extends JFrame {
         button0 = new JButton("0");
         buttonPlus = new JButton("+");
         buttonMinus = new JButton("-");
+        clearAll = new JButton("C");
 
-        thePanel.add(button1);
-        thePanel.add(button2);
-        thePanel.add(button3);
-        thePanel.add(button4);
-        thePanel.add(button5);
-        thePanel.add(button6);
-        thePanel.add(button7);
-        thePanel.add(button8);
-        thePanel.add(button9);
-        thePanel.add(button0);
-        thePanel.add(buttonPlus);
-        thePanel.add(buttonMinus);
+        thePanel.add(clearAll, gridBagConstraints);
+        gridBagConstraints.gridwidth = 20;
+        gridBagConstraints.gridx = 5;
+        thePanel.add(textResult, gridBagConstraints);
+        gridBagConstraints.gridwidth = 1;
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 2;
+        thePanel.add(button1, gridBagConstraints);
+        gridBagConstraints.gridx = 5;
+        thePanel.add(button2, gridBagConstraints);
+        gridBagConstraints.gridx = 9;
+        thePanel.add(button3, gridBagConstraints);
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 3;
+        thePanel.add(button4, gridBagConstraints);
+        gridBagConstraints.gridx = 5;
+        thePanel.add(button5, gridBagConstraints);
+        gridBagConstraints.gridx = 9;
+        thePanel.add(button6, gridBagConstraints);
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 4;
+        thePanel.add(button7, gridBagConstraints);
+        gridBagConstraints.gridx = 5;
+        thePanel.add(button8, gridBagConstraints);
+        gridBagConstraints.gridx = 9;
+        thePanel.add(button9, gridBagConstraints);
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 5;
+        thePanel.add(button0, gridBagConstraints);
+        gridBagConstraints.gridx = 5;
+        thePanel.add(buttonPlus, gridBagConstraints);
+        gridBagConstraints.gridx = 9;
+        thePanel.add(buttonMinus, gridBagConstraints);
+
 
         // Panel close up
         this.add(thePanel);
